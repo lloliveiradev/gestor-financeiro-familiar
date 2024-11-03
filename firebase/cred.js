7d72fbc7dd9ddde3a88fa123242d461914ab439e
Leo Oliveira <135054535+lloliveiradev@users.noreply.github.com>
2024-11-03 18:21:08 -0300

    Delete firebase directory

--- firebase/cred.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    apiKey: "AIzaSyCFVsg42O7rX5fhwO2I4tyShnAr1k72JNw",
-    authDomain: "gestor-financeiro-familiar.firebaseapp.com",
-    projectId: "gestor-financeiro-familiar",
-    storageBucket: "gestor-financeiro-familiar.firebasestorage.app",
-    messagingSenderId: "555591106388",
-    appId: "1:555591106388:web:98d3a55379b581014592c1"
-};
